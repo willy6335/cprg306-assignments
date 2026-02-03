c38026735b5c0fa835652ed1f38814301dbb7ab6
willy6335 <willardsunil01@gmail.com>
2026-02-02 20:17:36 -0700

    Week 2 assigment complete

---
+++ cprg306-assignments/app/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+
+      <Link href="/week-2">Go to Week 2</Link>
+
+      <p>Student: Willard Sunil</p>
+    </main>
+  );
+}
---
+++ cprg306-assignments/app/week-2/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Willard Sunil</p>
+      <Link href="https://github.com/willy6335/cprg306-assignments">
+        https://github.com/willy6335/cprg306-assignments
+      </Link>
+    </div>
+  );
+}
