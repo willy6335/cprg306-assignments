f58b6381adce8510970f0e86cbfa57955a9961e5
willy6335 <willardsunil01@gmail.com>
2026-04-03 20:48:30 -0600

    fix week 10 page build error

--- app/week-10/page.js
+++ app/week-10/page.js
@@ -1,8 +1,11 @@
 export default function Week10() {
     return (
-      <div>
+      <div style={{ padding: "20px" }}>
         <h1>Week 10</h1>
-        <a href="/week-10/shopping-list">Go to Shopping List</a>
+  
+        <a href="/week-10/shopping-list">
+         
+        </a>
       </div>
     );
   }
