f256758a7c5e68e0646d08d6cdfd26fad9757d6b
willy6335 <willardsunil01@gmail.com>
2026-04-17 04:53:58 -0600

    fix week 10 redirect

--- app/week-10/page.js
+++ app/week-10/page.js
@@ -1,11 +1,5 @@
-export default function Week10() {
-    return (
-      <div style={{ padding: "20px" }}>
-        <h1>Week 10</h1>
-  
-        <a href="/week-10/shopping-list">
-         
-        </a>
-      </div>
-    );
-  }
+import { redirect } from "next/navigation";
+
+export default function Week10Page() {
+  redirect("/week-10/shopping-list");
+}
